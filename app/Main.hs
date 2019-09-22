8cc3b11993dd79cd2a0751f3a63d10a771c6de1a
Julien Grimault <grimault.julien@gmail.com>
2019-09-22 13:38:32 -0700

    Add images copy rule

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import Control.Monad
+import Development.Shake
+import Development.Shake.FilePath
+import Slick
+
+outputDirectory:: FilePath
+outputDirectory = "./docs"
+
+site :: FilePath
+site = "./site"
+
+copyStaticFiles:: Action ()
+copyStaticFiles = 
+    in do
+    assetPaths <- getDirectoryFiles site ["images/*"]
+    void $ forP assetPaths $ \path ->
+         copyFileChanged (site </> path) (outputDirectory </> path)
 
 main :: IO ()
-main = someFunc
+main = slick copyStaticFiles
